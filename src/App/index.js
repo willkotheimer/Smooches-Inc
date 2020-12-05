491445000dfbd59b845b8eaf034526fe24b029dc
WillKotheimer <wkotheimer@gmail.com>
2020-12-05 10:26:47 -0600

    added authentication

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import './App.scss';
+import firebase from 'firebase/app';
+import fbConnection from '../helpers/data/connection';
+import Auth from '../components/Auth';
+import MyNavbar from '../components/Navbar';
+import Dashboard from '../components/Dashboard';
+
+fbConnection();
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  };
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      let component = '';
+      if (authed) {
+        component = <Dashboard />;
+      } else {
+        component = <Auth />;
+      }
+      return component;
+    };
+
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <MyNavbar authed={authed} />
+        {loadComponent()}
       </div>
     );
   }
